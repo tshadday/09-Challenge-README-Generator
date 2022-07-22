974c17d9d197a67b5f00f61eb9b7f3b1d5c1b825
Tanner Shadday <tshadday99@gmail.com>
2022-07-22 13:43:01 -0400

    fixed questions and added working function to ask questions

--- index.js
+++ index.js
@@ -9,60 +9,62 @@ const questions = [
     {
         type:"input",
         name:"Title",
-        input:"What is the Title of the project?",
+        message:"What is the Title of the project?",
     },
     {
         type:"input",
         name:"Description",
-        input:"Describe your project",
+        message:"Describe your project",
     },
     {
         type:"input",
         name:"Installation",
-        input:"How can people install your project?",
+        message:"How can people install your project?",
     },
     {
         type:"input",
         name:"Test",
-        input:"How can people test your project?",
+        message:"How can people test your project?",
     },
     {
         type:"input",
         name:"Usage",
-        input:"What should the user know about using your product?",
+        message:"What should the user know about using your product?",
     },
     {
         type:"list",
         name:"License",
-        input:"What license does your project have?",
+        message:"What license does your project have?",
         choices: ["Apache", "MIT", "BSD", "GPL", "None"]
     },
     {
         type:"input",
         name:"Contributing",
-        input:"What would you like to tell the people that wish to contribute?",
+        message:"What would you like to tell the people that wish to contribute?",
     },
     {
         type:"input",
         name:"Github",
-        input:"Enter your Github username.",
+        message:"Enter your Github username.",
     },
     {
         type:"input",
         name:"Email",
-        input:"Enter your email address.",
+        message:"Enter your email address.",
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-}
+    fs.writeFile(fileName, data)
+};
 
 // TODO: Create a function to initialize app
 function init() {
-
-}
+    inquirer.prompt(questions).then(response => {
+        writeToFile("README.md", response);
+    });
+};
 
 // Function call to initialize app
 init();
